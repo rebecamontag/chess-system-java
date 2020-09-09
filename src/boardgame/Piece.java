593142572cb45e3257edb56816694fe9692dd833
Rebeca Montagnana <montagrebeca@gmail.com>
2020-09-08 22:49:11 -0300

    Placing pieces on the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,16 +1,16 @@
 package boardgame;
 
 public class Piece {
-	
+
 	protected Position position;
 	private Board board;
-	
+
 	public Piece(Board board) {
 		this.board = board;
-		position  = null;
+		position = null;
 	}
 
-	protected Board getBoard() { 
+	protected Board getBoard() {
 		return board;
 	}
 
