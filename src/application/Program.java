542249ed3a7e0a088d862d5a628691f21485cd4a
Rebeca Montagnana <montagrebeca@gmail.com>
2020-09-12 00:04:45 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,7 +17,6 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
-		
 
 		while (!chessMatch.getCheckMate()) {
 			try {
@@ -35,9 +34,15 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
-				if(capturedPiece != null) {
+
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
+				}
+
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
 				}
 			}
 			catch (ChessException e) {
